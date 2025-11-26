987e2e179969dea545320359b924dd9ddb9b6721
srikanthg3734-lgtm <srikanthg3734@gmail.com>
2025-11-26 10:54:04 +0530

    Create IfElseDemo.java

---
+++ SOURCE/IfElseDemo.java
@@ -0,0 +1,9 @@
+class IfElseDemo {
+public static void main(String[] args) {
+int number = 10;
+if (number % 2 == 0)
+System.out.println(number + " is Even");
+else
+System.out.println(number + " is Odd");
+}
+}
